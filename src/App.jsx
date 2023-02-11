7bd388906dc41f6f21c84ff571127dd02e4a959b
TukanDev <contact@tukandev.com>
2023-02-11 03:15:16 +0100

    add particles
    Signed-off-by: TukanDev <contact@tukandev.com>

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import 'jquery/dist/jquery.min'
 import '@floating-ui/react'
 import 'bootstrap/dist/js/bootstrap.min';
 import 'react-toastify/dist/ReactToastify.css';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 import Register from "./components/pages/register";
 import ForgetPassword from "./components/pages/forgetPassword";
@@ -12,6 +14,8 @@ import ForgetPassword from "./components/pages/forgetPassword";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.particlesConfig = {"particles":{"number":{"value":10,"density":{"enable":true,"value_area":50}},"color":{"value":"#235712"},"shape":{"type":"star","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.4498212552380498,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":4,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":100,"color":"#235712","opacity":0.4,"width":0.6313280775270874},"move":{"enable":true,"speed":2.5,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":800}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"repulse"},"resize":false},"modes":{"grab":{"distance":150,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":100,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true}
 
     this.state = {
       page: 'register'
@@ -22,16 +26,22 @@ export default class App extends React.Component {
 
   render() {
     return(
-        <div className="d-flex justify-content-center align-items-center" style={{height: '100vh'}}>
-          { this.state.page === 'register' ? <Register navigation={this.navigation}/> : null }
-          {this.state.page === 'forgetPassword' ? <ForgetPassword navigation={this.navigation}/> : null }
-        </div>
-    )
+        <>
+          <Particles
+              id={"particles"}
+              init={this.particlesInit}
+              options={this.particlesConfig} />
+          <div className="d-flex justify-content-center align-items-center" style={{height: '100vh'}}>
+            { this.state.page === 'register' ? <Register navigation={this.navigation}/> : null }
+            {this.state.page === 'forgetPassword' ? <ForgetPassword navigation={this.navigation}/> : null }
+          </div>
+        </>
+  )
   }
 
   componentDidMount() {
     let page = window.location.href
-    if (page.includes('#/registe')) return this.setState({page: 'register'});
+    if (page.includes('#/register')) return this.setState({page: 'register'});
     if (page.includes('#/forgetPassword')) return this.setState({page: 'forgetPassword'});
   }
 
@@ -42,6 +52,10 @@ export default class App extends React.Component {
         this.setState({page: 'forgetPassword'})
         break
     }
+  }
+
+  async particlesInit(engine) {
+    await loadFull(engine);
   }
 
 }
